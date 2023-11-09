817e20e3cf5e36aa70b7ce44356c6c546d4dd7b1
Bill Melvin <bill@billmelvin.com>
2023-11-09 11:08:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# masht
+
+**M**arkdown **as** **HT**ML
+
+Reads a Markdown file and writes a HTML of the same name with a `AS.html` suffix.
---
+++ tests/test_md_to_html.py
@@ -0,0 +1,45 @@
+
+import pytest
+import sys
+
+from textwrap import dedent
+
+from masht import md_to_html
+
+
+# def test_failure():
+#     assert False
+
+
+def test_a_markdown_file(tmp_path, monkeypatch):
+    md = tmp_path / "test.md"
+    md.write_text(
+        dedent(
+            """
+            # Heading 1
+
+            ## Heading 2
+
+            ### Heading 3
+
+            [bogusoft](https://www.bogusoft.com/)
+
+            List of:
+            - stuff
+            - fluff
+            - snuff
+
+            ---
+
+            """
+        )
+    )
+
+    assert md.exists()
+
+    monkeypatch.setattr(sys, "argv", ["masht.py", str(md)])
+
+    md_to_html.main()
+
+    assert md.with_suffix(".md.AS.html").exists()
+
